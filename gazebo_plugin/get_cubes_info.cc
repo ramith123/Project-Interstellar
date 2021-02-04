6d65e30944a6b1f89ec026b55abbe8741d506d1c
SimplyTim <timo3447@gmail.com>
2021-02-04 00:57:53 -0400

    outputs location of only cubes

--- gazebo_plugin/get_cubes_info.cc
+++ gazebo_plugin/get_cubes_info.cc
@@ -2,6 +2,8 @@
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
+#include <ignition/math/Vector3.hh>
+#include <ignition/math/Quaternion.hh>
 #include <iostream>
 
 namespace gazebo
@@ -16,7 +18,24 @@ namespace gazebo
       modelCount = _world->ModelCount();
       for (int i=0; i<modelCount; i++){
         physics::ModelPtr m = _world->ModelByIndex(i);
-        std::cout << m->GetSDF()->ToString("");
+
+        // Get name of model/entity
+        std::string modelName =  m->GetSDF()->GetAttribute("name")->GetAsString();
+
+        // Only proceeds if the name of the model has the word "cube" or "box" in it.
+        if (modelName.find("box")!= std::string::npos||modelName.find("cube")!= std::string::npos){
+        // Get pose of model/entity
+        double x,y,z;
+        ignition::math::Pose3d pose;     
+        pose = m->WorldPose();
+        ignition::math::Vector3<double> v1(0, 0, 0);
+        v1 = pose.Pos();
+        x = v1.X(); // x coordinate
+        y = v1.Y(); // y coordinate
+        z = v1.Z(); // z coordinate
+
+        std::cout << modelName << ": x:" << x << " y:" << y << " z:" << z << "\n";
+        }
       }
 
     }

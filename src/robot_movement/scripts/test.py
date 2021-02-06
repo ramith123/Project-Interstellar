6d10e3e4a6c6db73cd1273c72b1e16bb173e0193
Kumar Etwaroo <adrian.etwaroo@gmail.com>
2021-02-06 11:03:45 -0400

    Node interacting with robot successfully

---
+++ src/robot_movement/scripts/test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+
+
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node('test', anonymous=True)
+
+robot = moveit_commander.RobotCommander()
+
+scene = moveit_commander.PlanningSceneInterface()
+
+group_name = "arm"
+move_group = moveit_commander.MoveGroupCommander(group_name)
+
+display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                               moveit_msgs.msg.DisplayTrajectory,
+                                               queue_size=20)
+
+# We can get the name of the reference frame for this robot:
+planning_frame = move_group.get_planning_frame()
+print("============ Planning frame: %s" % planning_frame)
+
+# We can also print the name of the end-effector link for this group:
+eef_link = move_group.get_end_effector_link()
+print("============ End effector link: %s" % eef_link)
+
+# We can get a list of all the groups in the robot:
+group_names = robot.get_group_names()
+print("============ Available Planning Groups:", robot.get_group_names())
+
+# Sometimes for debugging it is useful to print the entire state of the
+# robot:
+print("============ Printing robot state")
+print(robot.get_current_state())
+print("")

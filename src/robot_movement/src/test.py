0353cbde3cd503517eeb087b59572a9635e7c562
Kumar Etwaroo <adrian.etwaroo@gmail.com>
2021-02-15 10:47:29 -0400

    Succesffuly moved the robot using the Move Group Python Interface

--- src/robot_movement/src/test.py
+++ src/robot_movement/src/test.py
@@ -12,69 +12,94 @@ from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
 
 
-moveit_commander.roscpp_initialize(sys.argv)
-rospy.init_node('robot_movement', anonymous=True)
+class MoveGroupPythonIntefaceTutorial(object):
+    """MoveGroupPythonIntefaceTutorial"""
 
-robot = moveit_commander.RobotCommander()
+    def __init__(self):
+        super(MoveGroupPythonIntefaceTutorial, self).__init__()
+
+        # BEGIN_SUB_TUTORIAL setup
+        ##
+        # First initialize `moveit_commander`_ and a `rospy`_ node:
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node('robot_movement', anonymous=True)
+
+        # Instantiate a `RobotCommander`_ object. Provides information such as the robot's
+        # kinematic model and the robot's current joint states
+        robot = moveit_commander.RobotCommander()
+
+        # Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
+        # for getting, setting, and updating the robot's internal understanding of the
+        # surrounding world:
+        scene = moveit_commander.PlanningSceneInterface()
+
+        # Instantiate a `MoveGroupCommander`_ object.  This object is an interface
+        # to a planning group (group of joints).  In this tutorial the group is the primary
+        # arm joints in the Panda robot, so we set the group's name to "panda_arm".
+        # If you are using a different robot, change this value to the name of your robot
+        # arm planning group.
+        # This interface can be used to plan and execute motions:
+        group_name = "meca_arm"
+        move_group = moveit_commander.MoveGroupCommander(group_name)
+
+        # Create a `DisplayTrajectory`_ ROS publisher which is used to display
+        # trajectories in Rviz:
+        display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                                       moveit_msgs.msg.DisplayTrajectory,
+                                                       queue_size=20)
+
+        # END_SUB_TUTORIAL
+
+        # BEGIN_SUB_TUTORIAL basic_info
+        ##
+        # Getting Basic Information
+        # ^^^^^^^^^^^^^^^^^^^^^^^^^
+        # We can get the name of the reference frame for this robot:
+        planning_frame = move_group.get_planning_frame()
+        print("============ Planning frame: %s" % planning_frame)
+
+        # We can also print the name of the end-effector link for this group:
+        eef_link = move_group.get_end_effector_link()
+        print("============ End effector link: %s" % eef_link)
+
+        # We can get a list of all the groups in the robot:
+        group_names = robot.get_group_names()
+        print("============ Available Planning Groups:", robot.get_group_names())
+
+        # Sometimes for debugging it is useful to print the entire state of the
+        # robot:
+        print("============ Printing robot state")
+        print(robot.get_current_state())
+        print("")
+        # END_SUB_TUTORIAL
+
+        # Misc variables
+        self.box_name = ''
+        self.robot = robot
+        self.scene = scene
+        self.move_group = move_group
+        self.display_trajectory_publisher = display_trajectory_publisher
+        self.planning_frame = planning_frame
+        self.eef_link = eef_link
+        self.group_names = group_names
+
+    def go_to_pose_goal(self):
+
+        pose_goal = [0.0203439, 0.189837, 0.307987, -
+                     5.01149e-05, 1.45655e-05, 0.692353, 0.721559]
+
+        # move_group.set_pose_target(pose_goal)
+
+        self.move_group.set_pose_target(pose_goal, self.eef_link)
+
+        # Now, we call the planner to compute the plan and execute it.
+        plan = self.move_group.go(wait=True)
+        # Calling `stop()` ensures that there is no residual movement
+        self.move_group.stop()
+        # It is always good to clear your targets after planning with poses.
+        # Note: there is no equivalent function for clear_joint_value_targets()
+        self.move_group.clear_pose_targets()
 
 
-scene = moveit_commander.PlanningSceneInterface()
-
-group_name = "meca_arm"
-move_group = moveit_commander.MoveGroupCommander(group_name)
-
-display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
-                                               moveit_msgs.msg.DisplayTrajectory,
-                                               queue_size=20)
-
-
-# We can get the name of the reference frame for this robot:
-planning_frame = move_group.get_planning_frame()
-print("============ Planning frame: %s" % planning_frame)
-
-# We can also print the name of the end-effector link for this group:
-eef_link = move_group.get_end_effector_link()
-print("============ End effector link: %s" % eef_link)
-
-# We can get a list of all the groups in the robot:
-group_names = robot.get_group_names()
-print("============ Available Planning Groups:", robot.get_group_names())
-
-# Sometimes for debugging it is useful to print the entire state of the
-# robot:
-print("============ Printing robot state")
-print(robot.get_current_state())
-print("")
-
-
-joint_goal = move_group.get_current_joint_values()
-joint_goal[0] = 0
-joint_goal[1] = -pi/4
-joint_goal[2] = 0
-joint_goal[3] = -pi/2
-joint_goal[4] = 0
-joint_goal[5] = pi/3
-# joint_goal[6] = 0
-
-# The go command can be called with joint values, poses, or without any
-# parameters if you have already set the pose or joint target for the group
-move_group.go(joint_goal, wait=True)
-
-# Calling ``stop()`` ensures that there is no residual movement
-move_group.stop()
-
-
-pose_goal = geometry_msgs.msg.Pose()
-pose_goal.orientation.w = 1.0
-pose_goal.position.x = 0.4
-pose_goal.position.y = 0.1
-pose_goal.position.z = 0.4
-
-move_group.set_pose_target(pose_goal)
-
-plan = move_group.go(wait=True)
-# Calling `stop()` ensures that there is no residual movement
-move_group.stop()
-# It is always good to clear your targets after planning with poses.
-# Note: there is no equivalent function for clear_joint_value_targets()
-move_group.clear_pose_targets()
+tutorial = MoveGroupPythonIntefaceTutorial()
+tutorial.go_to_pose_goal()

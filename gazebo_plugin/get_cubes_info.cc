111af8baec293dd190e515da0947f6181be7fe0b
SimplyTim <timo3447@gmail.com>
2021-02-15 15:40:12 -0400

    cleaned up code a bit

--- gazebo_plugin/get_cubes_info.cc
+++ gazebo_plugin/get_cubes_info.cc
@@ -25,7 +25,6 @@ namespace gazebo
     {
       world = _world;
       sdf = _sdf;
-      //this->getLocations();
 
       // Initialize ros, if it has not already bee initialized.
       if (!ros::isInitialized())
@@ -41,59 +40,12 @@ namespace gazebo
 
       // Create a named topic, and subscribe to it.
       ros::Publisher sendCubeLocations = rosNode.advertise<std_msgs::String>("locations", 1000);
-      ros::Rate loop_rate(2);
-
-      /**
-       * A count of how many messages we have sent. This is used to create
-       * a unique string for each message.
-       */
-      int count = 0;
-      while (ros::ok())
-      {
-        cubeLocations = "\n";
-        modelCount = this->world->ModelCount();
-        for (int i=0; i<modelCount; i++){
-          physics::ModelPtr m = this->world->ModelByIndex(i);
-
-          // Get name of model/entity
-          std::string modelName =  m->GetSDF()->GetAttribute("name")->GetAsString();
-
-          // Only proceeds if the name of the model has the word "cube" or "box" in it.
-          if (modelName.find("box")!= std::string::npos||modelName.find("cube")!= std::string::npos){
-          // Get pose of model/entity
-          double x,y,z;
-          ignition::math::Pose3d pose;     
-          pose = m->WorldPose();
-          ignition::math::Vector3<double> v1(0, 0, 0);
-          v1 = pose.Pos();
-          x = v1.X(); // x coordinate
-          y = v1.Y(); // y coordinate
-          z = v1.Z(); // z coordinate
-
-          cubeLocations = cubeLocations + std::to_string(x) + "," + std::to_string(y) + "," + std::to_string(z) + "\n";
-          }
-        }
-
-        std_msgs::String msg;
-
-        std::stringstream ss;
-        ss << this->cubeLocations << count;
-        msg.data = ss.str();
-
-        ROS_INFO("%s", msg.data.c_str());
-        sendCubeLocations.publish(msg);
-
-        loop_rate.sleep();
-        ++count;
-      }
-    }
-/*
-    public: void getLocations(){
-      cubeLocations = "";
+      ros::Rate loop_rate(1);
+      cubeLocations = "\n";
       modelCount = this->world->ModelCount();
       for (int i=0; i<modelCount; i++){
         physics::ModelPtr m = this->world->ModelByIndex(i);
-
+        world->PublishModelPose(m);
         // Get name of model/entity
         std::string modelName =  m->GetSDF()->GetAttribute("name")->GetAsString();
 
@@ -112,9 +64,19 @@ namespace gazebo
         cubeLocations = cubeLocations + std::to_string(x) + "," + std::to_string(y) + "," + std::to_string(z) + "\n";
         }
       }
-      std::cout << cubeLocations;
+
+      while (ros::ok()){
+        std_msgs::String msg;
+
+        std::stringstream ss;
+        ss << this->cubeLocations;
+        msg.data = ss.str();
+
+        ROS_INFO("%s", msg.data.c_str());
+        sendCubeLocations.publish(msg);
+        loop_rate.sleep();
+      }
     }
-  */
   };
   GZ_REGISTER_WORLD_PLUGIN(GetCubesInfo)
 }

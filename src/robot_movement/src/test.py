18f264e3a8ddeaa727c17cd93d51b7794ca30285
Kumar Etwaroo <adrian.etwaroo@gmail.com>
2021-02-15 09:00:36 -0400

    Working on python node for robot movement

--- src/robot_movement/src/test.py
+++ src/robot_movement/src/test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+
 import sys
 import copy
 import rospy
@@ -12,18 +13,20 @@ from moveit_commander.conversions import pose_to_list
 
 
 moveit_commander.roscpp_initialize(sys.argv)
-rospy.init_node('test', anonymous=True)
+rospy.init_node('robot_movement', anonymous=True)
 
 robot = moveit_commander.RobotCommander()
 
+
 scene = moveit_commander.PlanningSceneInterface()
 
-group_name = "arm"
+group_name = "meca_arm"
 move_group = moveit_commander.MoveGroupCommander(group_name)
 
 display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
                                                moveit_msgs.msg.DisplayTrajectory,
                                                queue_size=20)
+
 
 # We can get the name of the reference frame for this robot:
 planning_frame = move_group.get_planning_frame()
@@ -42,3 +45,36 @@ print("============ Available Planning Groups:", robot.get_group_names())
 print("============ Printing robot state")
 print(robot.get_current_state())
 print("")
+
+
+joint_goal = move_group.get_current_joint_values()
+joint_goal[0] = 0
+joint_goal[1] = -pi/4
+joint_goal[2] = 0
+joint_goal[3] = -pi/2
+joint_goal[4] = 0
+joint_goal[5] = pi/3
+# joint_goal[6] = 0
+
+# The go command can be called with joint values, poses, or without any
+# parameters if you have already set the pose or joint target for the group
+move_group.go(joint_goal, wait=True)
+
+# Calling ``stop()`` ensures that there is no residual movement
+move_group.stop()
+
+
+pose_goal = geometry_msgs.msg.Pose()
+pose_goal.orientation.w = 1.0
+pose_goal.position.x = 0.4
+pose_goal.position.y = 0.1
+pose_goal.position.z = 0.4
+
+move_group.set_pose_target(pose_goal)
+
+plan = move_group.go(wait=True)
+# Calling `stop()` ensures that there is no residual movement
+move_group.stop()
+# It is always good to clear your targets after planning with poses.
+# Note: there is no equivalent function for clear_joint_value_targets()
+move_group.clear_pose_targets()

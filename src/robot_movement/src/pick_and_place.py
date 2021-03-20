65e644848a0093d5d404caf3e9e20b6ad84912ed
Kumar Etwaroo <adrian.etwaroo@gmail.com>
2021-03-20 10:14:50 -0400

    Added comments

--- src/robot_movement/src/pick_and_place.py
+++ src/robot_movement/src/pick_and_place.py
@@ -10,28 +10,44 @@ def mecademic_robot_basic_movement():
     moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node('simple_pick_place', anonymous=True)
 
+    # Instantiate a MoveGroupCommander object.  This object is an interface
+    # to one group of joints.  In this case the group refers to the joints of
+    # the meca_arm.
     meca_arm_group = MoveGroup("meca_arm")
+
+    # MoveGroup Commander Object for the mecademic hand.
     meca_fingers_group = MoveGroup("hand")
 
+    # Ensure that the robot begins at its home position
+    # Set a named joint configuration as the goal to plan for a move group.
+    # Named joint configurations are the robot poses defined via MoveIt! Setup Assistant.
     meca_arm_group.move_to_home()
 
+    # Ensure that the robot fingers are opened to pick up cube
     meca_fingers_group.move_via_joint_values([0.040, -1])
 
+    # Cartesian path movement to pre grasp position
     meca_arm_group.cartesian_movement([0.05, -999, -0.4])
 
+    # Close the mecademic robot fingers to pick cube up.
     meca_fingers_group.move_via_joint_values([0.00, -1])
 
-    meca_arm_group.move_via_joint_values([0, 0, 0, 0, 0, 0])
+    # Place the robot to its home position to begin place movement
+    meca_arm_group.move_to_home()
 
+    # Rotate the robot 90%
     meca_arm_group.move_via_joint_values([1.5708])
 
+    # Perform the pre-place movement
     meca_arm_group.cartesian_movement([-999, 0.05, -0.4])
 
+    # Open the mecademic robot fingers to place cube.
     meca_fingers_group.move_via_joint_values([0.040, -1])
 
-    meca_arm_group.cartesian_movement([-999, 0, 0.4])
+    # move robot up a bit to clear the cube
+    meca_arm_group.cartesian_movement([-999, 0, 0.3])
 
-    meca_arm_group.move_via_joint_values([0, 0, 0, 0, 0, 0])
+    meca_arm_group.move_to_home()
 
     # When finished shut down moveit_commander.
     moveit_commander.roscpp_shutdown()
